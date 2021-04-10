dc2465a084b437cd41161607951913d9a4ef62ff
Felipe Glautier <felipeglautier@gmail.com>
2021-04-10 17:04:37 -0300

    criando TableView

--- Investimentos/Investimentos/Scene/ViewController.swift
+++ Investimentos/Investimentos/Scene/ViewController.swift
@@ -1,20 +1,36 @@
-//
-//  ViewController.swift
-//  Investimentos
-//
-//  Created by Lipe glautier on 02/04/21.
-//
-
 import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TableView1: UITableView!
+    
+    var dice = ["Vamos", "Estudar", "Oque?", "Hoje", "?", "..."]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        TableView1.delegate =  self
+        TableView1.dataSource = self
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Você selecionou \(dice[indexPath.row]).")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dice.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TableView1.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath)
+        cell.textLabel?.text = dice[indexPath.row]
+        return cell
+        
+    }
+}
